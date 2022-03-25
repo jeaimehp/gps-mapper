2e8497a10c1bfb36fa2abcd6a19056473623ad7e
Je'aime Powell <jeaimehp@gmail.com>
2022-03-24 23:02:47 -0500

    filename style change

--- src/gps-mapper.ino
+++ src/gps-mapper.ino
@@ -230,11 +230,11 @@ void loop()
     }
     else if (buttonB){
       display.clearDisplay();
-      display.setTextSize(2);
-      display.setTextColor(WHITE);
-      display.setCursor(0,0);
-      display.println("Current File:");
+       display.setCursor(0,0);
       display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.println("Current File:");
+      display.setCursor(0,13);
       display.println(datafile_name);
       display.display();
     }
